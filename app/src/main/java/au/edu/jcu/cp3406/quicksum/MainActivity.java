98e05abf76b285ebe5c5c76ced09b2a42d2cc456
Dallas-Marshall <dallas.marshall@my.jcu.edu.au>
2021-03-10 10:58:01 +1000

    Add layout and event handler for clear button

--- app/src/main/java/au/edu/jcu/cp3406/quicksum/MainActivity.java
+++ app/src/main/java/au/edu/jcu/cp3406/quicksum/MainActivity.java
@@ -1,11 +1,11 @@
 package au.edu.jcu.cp3406.quicksum;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     private int sum;
@@ -23,6 +23,13 @@ public class MainActivity extends AppCompatActivity {
 
         TextView sumDisplay = findViewById(R.id.sum);
         String result = "" + sum;
+        sumDisplay.setText(result);
+    }
+
+    public void clearSumDisplay(View view) {
+        TextView sumDisplay = findViewById(R.id.sum);
+        sum = 0;
+        String result = "0";
         sumDisplay.setText(result);
     }
 }
