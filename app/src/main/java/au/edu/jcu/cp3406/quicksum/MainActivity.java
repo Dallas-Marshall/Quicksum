4e5df52a9f76eb7364678d1566e1f1fde5037066
Dallas-Marshall <dallas.marshall@my.jcu.edu.au>
2021-03-10 10:51:48 +1000

    Add event handler for keypad buttons

--- app/src/main/java/au/edu/jcu/cp3406/quicksum/MainActivity.java
+++ app/src/main/java/au/edu/jcu/cp3406/quicksum/MainActivity.java
@@ -3,12 +3,26 @@ package au.edu.jcu.cp3406.quicksum;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private int sum;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void buttonClicked(View view) {
+        Button button = (Button) view;
+        int number = Integer.parseInt(button.getText().toString());
+        sum += number;
+
+        TextView sumDisplay = findViewById(R.id.sum);
+        String result = "" + sum;
+        sumDisplay.setText(result);
     }
 }
